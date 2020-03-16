87dc1e87cf4897d4ecbf68f25551b2b906b0d432
Jakkaps <me@jakkaps.com>
2020-03-16 21:26:15 +0100

    Initial commit

---
+++ event/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """Print 'Hello, world!' as the response body."""
+    return 'Hello, world!'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'flask-sqlalchemy',
+    'psycopg2',
+]
+
+setup(
+    name='flask_todo',
+    version='0.0',
+    description='A To-Do List built with Flask',
+    author='Jens Kristoffersen',
+    author_email='jensakrr@gmail.com',
+    keywords='web flask',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
