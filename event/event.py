0c0398c8f437109cbf044cd76f6b89b76b293da7
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-18 18:40:38 +0100

    Added simple event class

---
+++ event/event.py
@@ -0,0 +1,8 @@
+class Event(object):
+    def __init__(self, name, description, datetime, host, location):
+        self.name = name
+        self.description = description
+        self.datetime = datetime
+        self.host = host
+        self.location = location
+        
