7557da6c5ec984f161132f511244129e6cec29d7
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-21 19:38:35 +0100

    fixed database setup and run

--- mysql/run_database.sh
+++ mysql/run_database.sh
@@ -1,1 +1,1 @@
-docker run --name event_db -e MYSQL_ROOT_PASSWORD=password -d event_db
+docker run --name event_db -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d event_db
--- mysql/setup_database.sql
+++ mysql/setup_database.sql
@@ -5,11 +5,11 @@ CREATE TABLE events (
        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
        name VARCHAR(100) NOT NULL,
        description VARCHAR(1000) NOT NULL,
-       start TIMESTAMP NOT NULL,
-       end TIMESTAMP NOT NULL,
+       start TIMESTAMP,
+       end TIMESTAMP,
        host VARCHAR(100) NOT NULL,
-       location VARCHAR(200) NOT NULL,
-       url VARCHAR(200) NOT NULL,
+       location VARCHAR(200),
+       url VARCHAR(200),
        study_program VARCHAR(200) NOT NULL,
-       class_year INTEGER NOT NULL
+       class_year INTEGER
 );
