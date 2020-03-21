c243d4ed5ca738f11f7f6666cf757a93c3363c99
Jakkaps <me@jakkaps.com>
2020-03-21 13:03:26 +0100

    Extended EventSpider to scrape Abakus.no

--- event/EventCrawler/EventCrawler/spiders/event_spider.py
+++ event/EventCrawler/EventCrawler/spiders/event_spider.py
@@ -15,7 +15,31 @@ class EventSpider(scrapy.Spider):
 
     
     def abakus_parse(self, response):
-        print(response.body) 
+        events = []
+        print(response)
+
+        for event in response.xpath("//descendant::div[@class='styles__eventItem--2c-Z_4PWb2']"):
+            url = "https://Abakus.no" + event.xpath("./div[1]/a/@href").get()
+            yield scrapy.Request(url=url, callback=self.abakus_parse_event)
+
+    def abakus_parse_event(self, response):
+        
+        description = ""
+        for paragraph in response.xpath("/descendant::span[@data-text='true']/text()"):
+            description += paragraph.get() + "\n"
+        
+
+        yield{
+            "url" : response.request.url,
+            "name" : response.xpath("/html/body/div/div/div/div[2]/div/div/div[2]/h2/text()").get(),
+            "type" : response.xpath("/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/ul/li[span[contains(text(), 'Hva')]]/strong/text()").get(),
+            "datetime" : response.xpath("/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/ul/li[span[contains(text(), 'Når')]]/strong/span/time/@datetime").get(),
+            "location" : response.xpath("/html/body/div/div/div/div[2]/div/div/div[2]/div[1]/div[2]/ul/li[span[contains(text(), 'Finner sted')]]/strong/text()").get(),
+            "description" : description[:-2],
+            "studyProgram" : "MTDT, MTKOM",
+            "host" : "Abakus",
+        }
+
 
 if __name__ == '__main__':
     spider = EventSpider()
