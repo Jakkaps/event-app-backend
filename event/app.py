759ee576ee3834402e235375bdf3243ba4e60220
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-21 10:47:19 +0100

    Moved endpoints to own file endpoints.py

--- event/app.py
+++ event/app.py
@@ -2,7 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    """Print 'Hello, world!' as the response body."""
-    return 'Hello, world!'
+import event.endpoints
