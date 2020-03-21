bf7e57d2ce2904e863ab2089190e1c79beea5634
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-21 11:20:39 +0100

    Added moch get_event endpoint

--- event/endpoints.py
+++ event/endpoints.py
@@ -1,6 +1,18 @@
 from event.app import app
+from flask import jsonify
+from event.event import Event
 
 
-@app.route("/")
-def base():
-    return "Hey there"
+
+@app.route('/get_events')
+def get_events():
+    """
+    Returns a list of all events from all datasources
+    """
+
+    # Moch event for testing
+    event = Event("AbelLan", "Kult lan for abakuler", "21.03.2020", "Abakus", "A2")
+    events = [event.__dict__]
+    
+
+    return jsonify(events)
