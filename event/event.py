562efe76ebe7587f36d493f243d49304e61cd0da
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-21 19:16:10 +0100

    Added build and start scripts for database container

--- event/event.py
+++ event/event.py
@@ -1,8 +1,12 @@
 class Event(object):
-    def __init__(self, name, description, datetime, host, location):
+    def __init__(self, name, description, start, end, host, location, url, study_program, class_year):
         self.name = name
         self.description = description
-        self.datetime = datetime
+        self.start = start
+        self.end = end
         self.host = host
         self.location = location
-        
+        self.url = url
+        self.study_program = study_program
+        self.class_year
+
---
+++ mysql/run_database.sh
@@ -0,0 +1,1 @@
+docker run --name event_db -e MYSQL_ROOT_PASSWORD=password -d event_db
