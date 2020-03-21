01ae11db54def9e084f2ccf72e0fdfe210913a82
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-21 19:39:20 +0100

    Did an oopsie in database setup, fixed that now

--- mysql/setup_database.sql
+++ mysql/setup_database.sql
@@ -9,7 +9,7 @@ CREATE TABLE events (
        end TIMESTAMP,
        host VARCHAR(100) NOT NULL,
        location VARCHAR(200),
-       url VARCHAR(200),
-       study_program VARCHAR(200) NOT NULL,
+       url VARCHAR(200) NOT NULL,
+       study_program VARCHAR(200),
        class_year INTEGER
 );
