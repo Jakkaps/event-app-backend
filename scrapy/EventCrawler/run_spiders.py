643a8fb9819cca84f75a924feaded423d29f27df
Jakkaps <jensakrr@gmail.com>
2020-03-25 22:10:27 +0100

    forgot to force scrapy to run with project settings when making it run via script

--- scrapy/EventCrawler/run_spiders.py
+++ scrapy/EventCrawler/run_spiders.py
@@ -1,12 +1,13 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 #Import all spiders
 from spiders import abakus_spider
 from spiders import samfundet_spider
 
 if __name__ == "__main__":
-    process = CrawlerProcess()
+    process = CrawlerProcess(get_project_settings())
     process.crawl(abakus_spider.AbakusSpider)
     process.crawl(samfundet_spider.SamfundetSpider)
     process.start()
